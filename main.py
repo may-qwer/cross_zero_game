9c9deb99417e15fa60419125cb679185ed370580
may-qwer <may0220@yandex.ru>
2023-05-20 00:09:37 +0300

    working death screen, create cross,
    make the screen of death through if k is greater than 9 then death and not through if the array length is less than 0

--- main.py
+++ main.py
@@ -8,55 +8,49 @@ CIRCLES_COORD = [[200, 200],
                  [200, 400],
                  [400, 200],
                  [400, 400]]
+CELL_POS = [[[0, 0], [200, 200]],
+                  [[202, 0], [400, 200]],
+                  [[402, 0], [600, 200]],
+                  [[0, 202], [200, 400]],
+                  [[202, 202], [400, 400]],
+                  [[402, 202], [600, 400]],
+                  [[0, 402], [200, 600]],
+                  [[202, 402], [400, 600]],
+                  [[402, 402], [600, 600]]]
+cells_pos = []
+
 
 class Player:
-    def __init__(self, mouse_pos):
-        # [[firstX, firstY], [lastX, lastY]] firstX < pos_mouse < lastX
-        # pos_mouse = (150, 150)
-
-        self.cells_pos = [[[0, 0], [200, 200]],
-                                [[202, 0], [400, 200]],
-                                [[402, 0], [600, 200]],
-                                [[0, 202], [200, 400]],
-                                [[202, 202], [400, 400]],
-                                [[402, 202], [600, 400]],
-                                [[0, 402], [200, 600]],
-                                [[202, 402], [400, 600]],
-                                [[402, 402], [600, 600]]]
+    def __init__(self, mouse_pos, window, cells_pos):
+        self.cells_pos = cells_pos
         self.mouse_pos = mouse_pos
-
-
+        self.window = window
+        self.game_end = False
 
     def create_cross(self):
-        pass
+        for cell in self.cells_pos:
+            if cell[0][0] <= self.mouse_pos[0] and cell[1][0] >= self.mouse_pos[0] and cell[0][1] <= self.mouse_pos[1] and cell[1][1] >= self.mouse_pos[1]:
+                pygame.draw.line(self.window, [57, 50, 255], (cell[0][0] + 40, cell[0][1] + 40), (cell[1][0] - 40, cell[1][1] - 40), 6)
+                pygame.draw.line(self.window, [57, 50, 255], (cell[0][0] + 160, cell[0][1] + 40), (cell[1][0] - 160, cell[1][1] - 40), 6)
+                self.cells_pos.remove(cell)
 
-    def check_cell(self):
-        if self.cells_pos[0][0][0] <= self.mouse_pos[0] and self.cells_pos[0][1][0] >= self.mouse_pos[0] and self.cells_pos[0][0][1] <= self.mouse_pos[1] and self.cells_pos[0][1][1] >= self.mouse_pos[1]:
-            print('1 cell')
-        elif self.cells_pos[1][0][0] <= self.mouse_pos[0] and self.cells_pos[1][1][0] >= self.mouse_pos[0] and self.cells_pos[1][0][1] <= self.mouse_pos[1] and self.cells_pos[1][1][1] >= self.mouse_pos[1]:
-            print('2 cell')
-        elif self.cells_pos[2][0][0] <= self.mouse_pos[0] and self.cells_pos[2][1][0] >= self.mouse_pos[0] and self.cells_pos[2][0][1] <= self.mouse_pos[1] and self.cells_pos[2][1][1] >= self.mouse_pos[1]:
-            print('3 cell')
-        elif self.cells_pos[3][0][0] <= self.mouse_pos[0] and self.cells_pos[3][1][0] >= self.mouse_pos[0] and self.cells_pos[3][0][1] <= self.mouse_pos[1] and self.cells_pos[3][1][1] >= self.mouse_pos[1]:
-            print('4 cell')
-        elif self.cells_pos[4][0][0] <= self.mouse_pos[0] and self.cells_pos[4][1][0] >= self.mouse_pos[0] and self.cells_pos[4][0][1] <= self.mouse_pos[1] and self.cells_pos[4][1][1] >= self.mouse_pos[1]:
-            print('5 cell')
-        elif self.cells_pos[5][0][0] <= self.mouse_pos[0] and self.cells_pos[5][1][0] >= self.mouse_pos[0] and self.cells_pos[5][0][1] <= self.mouse_pos[1] and self.cells_pos[5][1][1] >= self.mouse_pos[1]:
-            print('6 cell')
-        elif self.cells_pos[6][0][0] <= self.mouse_pos[0] and self.cells_pos[6][1][0] >= self.mouse_pos[0] and self.cells_pos[6][0][1] <= self.mouse_pos[1] and self.cells_pos[6][1][1] >= self.mouse_pos[1]:
-            print('7 cell')
-        elif self.cells_pos[7][0][0] <= self.mouse_pos[0] and self.cells_pos[7][1][0] >= self.mouse_pos[0] and self.cells_pos[7][0][1] <= self.mouse_pos[1] and self.cells_pos[7][1][1] >= self.mouse_pos[1]:
-            print('8 cell')
-        elif self.cells_pos[8][0][0] <= self.mouse_pos[0] and self.cells_pos[8][1][0] >= self.mouse_pos[0] and self.cells_pos[8][0][1] <= self.mouse_pos[1] and self.cells_pos[8][1][1] >= self.mouse_pos[1]:
-            print('9 cell')
+    def end_game_screen(self):
+        if len(self.cells_pos) == 0:
+            self.game_end = True
+            self.window.fill((20, 20, 20))
+            self.window.blit(pygame.font.Font(None, 80).render('Game end!', True, (26, 255, 255)), (150, 220))
+            self.window.blit(pygame.font.Font(None, 40).render('Press SPACE to restart. Press ESC to quit.', True, (19, 137, 155)), (30, 290))
+
 
 def main():
     window = pygame.display.set_mode((600, 600))
     pygame.display.set_caption('Tic Tac Toe Game')
     clock = pygame.time.Clock()
+    pygame.font.init()
     window.fill((0, 0, 0))
+    cells_pos = CELL_POS
     for line in LINE_COORD:
-        pygame.draw.line(window, (255, 255, 255), line[0], line[1], 1)
+        pygame.draw.line(window, (255, 255, 255), line[0], line[1], 2)
     for circle in CIRCLES_COORD:
         pygame.draw.circle(window, [0, 0, 0], circle, 20)
     running = True
@@ -64,10 +58,17 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    cells_pos = CELL_POS
+                    main()
+                elif event.key == pygame.K_ESCAPE:
+                    running = False
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 mouse_pos = pygame.mouse.get_pos()
-                player = Player(mouse_pos)
-                player.check_cell()
+                player = Player(mouse_pos, window, cells_pos)
+                player.create_cross()
+                player.end_game_screen()
         pygame.display.update()
         clock.tick(30)
     pygame.quit()
--- test.py
+++ test.py
@@ -5,6 +5,7 @@ size = (800, 600)
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("My Game")
 
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
